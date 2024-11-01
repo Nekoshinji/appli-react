be56015fdf66ddb0b73dec3554b930742b0116f7
Nekoshinji <obito26@gmail.com>
2024-11-01 10:53:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import EmployeeCard from "./Compenents/EmployeeCard";
+
+const sampleEmployee = {
+	name: {
+		first: "Jorge",
+
+		last: "Nieto",
+	},
+
+	email: "jorge.nieto@example.com",
+
+	picture: {
+		medium: "https://randomuser.me/api/portraits/med/men/84.jpg",
+	},
+};
+
+function App() {
+	const [employee, setEmployee] = useState(sampleEmployee);
+	const getEmployee = () => {
+		// Send the request
+		fetch("http://localhost:3310/api/employees")
+			.then((response) => response.json())
+			.then((data) => {
+				setEmployee(data[0]);
+			})
+			.catch((error) => console.error(error));
+	};
+	[];
+
+	return (
+		<div className="App">
+			<EmployeeCard employee={employee} />
+			<button type="button" onClick={getEmployee}>
+				Get employee
+			</button>
+		</div>
+	);
+}
+
+export default App;
